09c79cf4de2eecf7546c817c5a216c5acc25c505
ProvidedPlay <mosesreid@hotmail.com>
2024-05-11 14:04:22 -0400

    added 'click me' button that randomizes the game board, added 'shuffle array', 'unpack array', 'add playable cards to game board', and 'setup game board' logic

--- src/components/GameBoard/index.js
+++ src/components/GameBoard/index.js
@@ -1,16 +1,16 @@
 import { Grid, VStack, Heading } from "@chakra-ui/react"
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import CharacterCard from "../CharacterCard"
 import characters from "../../characters.json"
+import { useForceUpdate } from "framer-motion";
 
 const importedCardArray = characters;
 
-
-const RenderCard = (cardData) => {
+const renderCard = (cardData, key) => {
 
     return(
         <CharacterCard
-        key = {cardData.title}
+        key = {cardData.title + key}
         title= {cardData.title}
         imageSrc = {cardData.imageSrc}
         />
@@ -18,31 +18,35 @@ const RenderCard = (cardData) => {
     )
 }
 
-
 const GameBoard = () =>{
     const [playableCards, setPlayableCards] = useState(importedCardArray);
     const [cardIndexArray, setCardIndexArray] = useState([])
+    const [state, updateState] = useState()
 
-    useEffect( () => {
-        setPlayableCards(importedCardArray)
-        console.log(playableCards)
-        UnpackCardIndexArray(8,2)
-        ShuffleCardIndexArray()
-    }, [cardIndexArray, playableCards])
+    const forceUpdate = useCallback(() => updateState({}), [])
 
-    const UnpackCardIndexArray = (numOfCardGroups, numOfTimesRepeated) =>
+    function handleClick() {
+        setUpGameBoard()
+        forceUpdate()
+    }
+    
+    const setUpGameBoard = () =>{
+        unpackCardIndexArray(8,2)
+        shuffleCardIndexArray()
+    }
+
+    const unpackCardIndexArray = (numOfCardGroups, numOfTimesRepeated) =>
         {
             let newCardIndexArray = []
             for(let i=1; i<= numOfTimesRepeated; i++){
                 newCardIndexArray.push.apply(newCardIndexArray,[...Array(numOfCardGroups).keys()])
             }
             setCardIndexArray(newCardIndexArray)
-            console.log(newCardIndexArray)
-            console.log(cardIndexArray)
         }
        
     
-    const ShuffleCardIndexArray = () => {
+    const shuffleCardIndexArray = () => {
+        
         let newCardIndexArray = cardIndexArray;
         
         for(let i= newCardIndexArray.length-1; i>0; i--){
@@ -51,16 +55,16 @@ const GameBoard = () =>{
             newCardIndexArray[i] = newCardIndexArray[randomIndex]
             newCardIndexArray[randomIndex] = currentValue
         }
-
-        setCardIndexArray(newCardIndexArray);
-        console.log(cardIndexArray)
+        if(cardIndexArray.length>0){
+            setCardIndexArray(newCardIndexArray)
+            console.log(cardIndexArray)
+        }
 
     }
 
-    const AddPlayableCardsToGameBoard = () => {
-        //console.log(importedCardArray)
+    const addPlayableCardsToGameBoard = () => {
         return(
-            cardIndexArray.map((index) => (RenderCard(playableCards[cardIndexArray[index]])))
+            cardIndexArray.map((index, key) => (renderCard(playableCards[index], key)))
         )
     }
     
@@ -69,8 +73,9 @@ const GameBoard = () =>{
             <Heading as="h1" color="white" height="8%">
                 Game Board
             </Heading>
+            <button onClick = {handleClick}>Start!</button>
             <Grid templateColumns="repeat(4,1fr)" gap={3} minHeight="100vh">
-                {AddPlayableCardsToGameBoard()}
+                {addPlayableCardsToGameBoard()}
             </Grid>
         </VStack>
 
